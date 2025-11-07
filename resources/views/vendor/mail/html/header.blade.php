0e473c0bc2051699bc29b8586b31a469c1304b89
AB <antonyb2004@hotmail.com>
2025-11-06 23:39:00 -0500

    fix: Correct logo image source to use application URL in email header

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-flex; align-items: center; gap: 12px;">
-            <img src="{{ config('app.url') }}/images/logo.svg" alt="{{ config('app.name') }}" style="height: 40px;" />
+            <img src="{{ url('/images/logo.svg') }}" alt="{{ config('app.name') }}" style="height: 40px;" />
             <span style="font-size: 18px; font-weight: 600; color: #111827;">{{ config('app.name') }}</span>
         </a>
     </td>
