f73680c02150f3bc892f720bcaa5a4bbc0acc9fe
AB <antonyb2004@hotmail.com>
2025-11-06 23:43:16 -0500

    fix: Simplify header link styling and remove logo image from email header

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,7 @@
 @props(['url'])
 <tr>
     <td class="header">
-        <a href="{{ $url }}" style="display: inline-flex; align-items: center; gap: 12px;">
-            <img src="{{ url('/images/logo.svg') }}" alt="{{ config('app.name') }}" style="height: 40px;" />
+        <a href="{{ $url }}" style="display: inline-block;">
             <span style="font-size: 18px; font-weight: 600; color: #111827;">{{ config('app.name') }}</span>
         </a>
     </td>
